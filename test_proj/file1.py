f7f64cfd89c6d290f45cc1b53060da133b113ca8
OmkarKabadagi5823 <kabadagiomkar@gmail.com>
2022-10-20 19:13:45 +0530

    track file1 temporarily

---
+++ test_proj/file1.py
@@ -0,0 +1,22 @@
+def func1():
+    pass
+
+
+def func2():
+    pass
+
+# !dco
+# def func3():
+#     pass
+# !dcc
+
+def func3_2():
+    pass
+
+# # !dco
+# def func4():
+#     pass
+# # dcc
+
+def func4_2():
+    pass
