84b7fd714f31bcbf25b9b9450aeaea88639ab5f0
OmkarKabadagi5823 <kabadagiomkar@gmail.com>
2022-10-23 23:27:05 +0530

    remove this in future

--- src/api/db/storage.rs
+++ src/api/db/storage.rs
@@ -32,16 +32,51 @@ impl Storage {
         &self.dc_dir
     }
     
-    pub fn compare_and_add(&self, _file_path: &path::PathBuf, comment_vec: Vec<CommentStamped>) {
+    pub fn compare_and_add(&self, file_path: &path::PathBuf, comment_vec: Vec<CommentStamped>) {
         let compressed = Storage::serialize_and_compress(comment_vec);
         
+        let hash = Storage::hash(&compressed);
+
         let mut bin_file = fs::File::options()
             .create_new(true)
             .write(true)
-            .open(self.proj_dir.join(dir::OBJECTS_PATH).join(Storage::hash(&compressed)))
+            .open(self.proj_dir.join(dir::OBJECTS_PATH).join(&hash))
             .unwrap();
 
         bin_file.write_all(&compressed).unwrap();
+
+        self.update_index(file_path, &hash);
+    }
+
+    pub fn populate(&self) {
+        let index_file = fs::File::options()
+            .read(true)
+            .open(self.proj_dir.join(dir::INDEX_PATH))
+            .unwrap();
+
+        let mut index_reader = BufReader::new(index_file);
+        let mut buf = String::new();
+
+        while index_reader.read_line(&mut buf).unwrap() > 0 {
+            let mut index_entry = buf.trim().split_whitespace();
+            let file_path_rel_proj = index_entry.next().unwrap();
+            let hash = index_entry.next().unwrap();
+            println!("{}: {}", file_path_rel_proj, hash);
+
+            let bin_file = fs::File::options()
+                .read(true)
+                .open(self.proj_dir.join(dir::OBJECTS_PATH).join(&hash))
+                .unwrap();
+
+            let mut bin_reader = BufReader::new(bin_file);
+            let mut compressed = Vec::new();
+            bin_reader.read_to_end(&mut compressed).unwrap();
+
+            let decompressed = Storage::decompress_and_deserialize(&compressed);
+            println!("{:#?}", decompressed);
+
+            buf.clear();
+        }
     }
 
     fn serialize_and_compress(comment_vec: Vec<CommentStamped>) -> Vec<u8> {
@@ -49,9 +84,29 @@ impl Storage {
         zstd::encode_all(serialized.as_slice(), 3).unwrap()
     }
 
+    fn decompress_and_deserialize(compressed: &Vec<u8>) -> Vec<CommentStamped> {
+        let decompressed = zstd::decode_all(compressed.as_slice()).unwrap();
+        let comment_vec = bincode::deserialize(&decompressed).unwrap();
+        comment_vec
+    }
+
     fn hash(data: impl AsRef<[u8]>) -> String {
         let mut hasher = Sha1::new();
         hasher.update(data);
         format!("{:x}", hasher.finalize())
+    }
+
+    fn update_index(&self, file_path: &path::PathBuf, hash: &str) {
+        let mut index_file = fs::File::options()
+            .create(true)
+            .append(true)
+            .open(self.proj_dir.join(dir::INDEX_PATH))
+            .unwrap();
+
+        let file_path_rel_proj = file_path.strip_prefix(&self.proj_dir).unwrap();
+
+        let index_entry = format!("{} {}\n", file_path_rel_proj.display(), hash);
+
+        index_file.write_all(index_entry.as_bytes()).unwrap();
     }
 }
