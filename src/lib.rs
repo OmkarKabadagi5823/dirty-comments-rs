4dfeb7d55ad5677ccf4294c061bf17cca4265a0c
OmkarKabadagi5823 <kabadagiomkar@gmail.com>
2022-10-23 23:24:30 +0530

    move consts to lib

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,11 @@ pub mod dir {
     pub const DC_DIR_PATH: &'static str  = ".dirty_comments";
     pub const INDEX_PATH: &'static str = ".dirty_comments/index";
     pub const OBJECTS_PATH: &'static str = ".dirty_comments/objects";
+    pub const TMP_FILE_PATH: &'static str = "/tmp/dirty_comments_tmp";
+}
+
+pub mod tags {
+    pub const OPENING: &'static str = "dco";
+    pub const CLOSING: &'static str = "dcc";
+    pub const MARKER: &'static str = "dcm";
 }
